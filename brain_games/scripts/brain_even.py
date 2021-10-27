30735f12f373ea26333618c71ecb11bc1ebe94b0
ghost-of-karelia <mitrijpavlov@gmail.com>
2021-10-27 16:15:46 +0300

    Initiate If Even Game

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,16 @@
+"""Play the If-Even Game."""
+
+import prompt 
+from brain_games import main
+
+
+main()
+
+def if_even():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    i = 0
+    while i <= 2: 
+
+
+
+if_even()
